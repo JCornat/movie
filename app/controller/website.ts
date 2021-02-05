f889611a32f96818d7fde1590389ec35a60a25be
Jacques Cornat <cornat.jacques@gmail.com>
2021-02-05 22:00:25 +0100

    fix asset location

--- app/controller/website.ts
+++ app/controller/website.ts
@@ -6,7 +6,7 @@ export const router = Router();
 
 router.get(/^(?!\/api.*).*/, async (req: Request, res: C7zResponse, next: any) => {
   try {
-    const filePath = path.join(__dirname, '..', '..', 'www', 'index.html');
+    const filePath = path.join(__dirname, '..', 'www', 'index.html');
     await res.sendFileAsync(filePath);
   } catch (error) {
     console.error(error);
--- app/middleware/asset.ts
+++ app/middleware/asset.ts
@@ -5,3 +5,5 @@ export const app = express();
 
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 app.use('/upload', express.static(path.join(__dirname, '..', 'public', 'image')));
+app.use('/www', express.static(path.join(__dirname, '..', 'www')));
+app.use('/assets', express.static(path.join(__dirname, '..', 'www', 'assets')));
