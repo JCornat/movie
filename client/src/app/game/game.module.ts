d7200245f66ec47c990eee084557efd995605303
Jacques Cornat <cornat.jacques@gmail.com>
2023-02-05 15:27:36 +0100

    start game refactor

--- client/src/app/game/game.module.ts
+++ client/src/app/game/game.module.ts
@@ -3,20 +3,20 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { GameComponent } from '@app/game/game.component';
 import { GameAddComponent } from '@app/game/add/add.component';
 import { GameImportComponent } from '@app/game/import/import.component';
-import { GameItemComponent } from '@app/game/item/item.component';
 import { GameRoutingModule } from '@app/game/game-routing.module';
 import { GameUpdateComponent } from '@app/game/update/update.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MediaModule } from "@app/media/media.module";
 
 @NgModule({
   declarations: [
     GameComponent,
     GameAddComponent,
     GameImportComponent,
-    GameItemComponent,
     GameUpdateComponent,
   ],
   imports: [
+    MediaModule,
     CommonModule,
     ReactiveFormsModule,
     GameRoutingModule,
--- client/src/app/media/media.module.ts
+++ client/src/app/media/media.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { MediaItemComponent } from "@app/media/item/item.component";
 
 @NgModule({
   declarations: [
-
+    MediaItemComponent,
+  ],
+  exports: [
+    MediaItemComponent,
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NgOptimizedImage,
+  ],
 })
 export class MediaModule { }
