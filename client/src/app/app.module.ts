f63439b8bf92bffa7bcb45472f1131e062b87ca7
Jacques Cornat <cornat.jacques@gmail.com>
2023-05-06 01:20:11 +0200

    add meta tags

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,27 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from '@app/home/home.component';
-import { LoginComponent } from '@app/login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    HomeComponent,
     HttpClientModule,
-    LoginComponent,
-    NgOptimizedImage,
-    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  //
 }
