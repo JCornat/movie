6bf129fbd056d9a26d2cd5a72ab5571d4afe57c8
Jacques Cornat <cornat.jacques@gmail.com>
2023-05-06 02:19:17 +0200

    update title

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,29 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import * as Config from '@shared/config/config';
+
 const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+    title: `${Config.TITLE}`,
   },
   {
     path: 'login',
     loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
+    title: `${Config.TITLE}`,
   },
   {
     path: 'game',
-    title: 'Games',
-    loadChildren: () => import('./game/routes').then(m => m.GAME_ROUTES)
+    loadChildren: () => import('./game/routes').then(m => m.GAME_ROUTES),
+    title: `${Config.TITLE} - Games`,
   },
   {
     path: 'movie',
-    title: 'Movies',
-    loadChildren: () => import('./movie/routes').then(m => m.MOVIE_ROUTES)
+    loadChildren: () => import('./movie/routes').then(m => m.MOVIE_ROUTES),
+    title: `${Config.TITLE} - Movies`,
   },
   {
     path: 'serie',
-    title: 'Series',
-    loadChildren: () => import('./serie/routes').then(m => m.SERIE_ROUTES)
+    loadChildren: () => import('./serie/routes').then(m => m.SERIE_ROUTES),
+    title: `${Config.TITLE} - Series`,
   },
 ];
 
