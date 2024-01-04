a23fa620e7331d3f1828b9725eb022f8b7758c6f
Jacques Cornat <cornat.jacques@gmail.com>
2024-01-04 13:56:18 +0100

    fix asset directory

--- app/middleware/asset.ts
+++ app/middleware/asset.ts
@@ -5,6 +5,6 @@ export const app = express();
 
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 app.use('/upload', express.static(path.join(__dirname, '..', 'public', 'image')));
-app.use('/', express.static(path.join(__dirname, '..', '..', '..', '..', 'www')));
+app.use('/', express.static(path.join(__dirname, '..', 'www')));
 app.use('/www', express.static(path.join(__dirname, '..', 'www')));
 app.use('/assets', express.static(path.join(__dirname, '..', 'www', 'assets')));
