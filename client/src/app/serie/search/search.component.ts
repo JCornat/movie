6cbb2a042ce68e0f9c26fe371172901bb54defe2
Jacques Cornat <cornat.jacques@gmail.com>
2024-01-14 14:45:47 +0100

    add decoration button directive

--- client/src/app/serie/search/search.component.ts
+++ client/src/app/serie/search/search.component.ts
@@ -1,16 +1,16 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { MediaSearchComponent } from '@app/media/search/search.component';
 import { SerieService } from '@app/serie/serie.service';
 import { FormInputDirective } from '@shared/form-input/form-input.directive';
+import { SharedModule } from '@shared/shared.module';
+import { MediaItemComponent } from '@app/media/item/item.component';
 
 @Component({
   selector: 'serie-search',
   templateUrl: '../../media/search/search.component.html',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormInputDirective],
+  imports: [SharedModule, MediaItemComponent],
 })
 export class SerieSearchComponent extends MediaSearchComponent {
   mediaService = inject(SerieService);
---
+++ client/src/app/shared/decoration/button.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, inject, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[decorationButton]',
+  standalone: true,
+})
+export class DecorationButtonDirective {
+  renderer = inject(Renderer2);
+  hostElement = inject(ElementRef);
+
+  constructor() {
+    const classNames = [
+      'w-full',
+      'bg-transparent',
+      'border',
+      'border-white/30',
+      'h-8',
+      'px-2.5',
+      'rounded-lg',
+      'text-sm',
+    ];
+
+    for (const className of classNames) {
+      this.renderer.addClass(this.hostElement.nativeElement, className);
+    }
+  }
+}
