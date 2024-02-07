afe736d2309644dc71aa6e0518351581c71bbe94
JulienMichelFr <julienmichel.pro@gmail.com>
2024-02-07 21:25:30 +0100

    refactor(client): simplify decoration directives

--- client/src/app/shared/decoration/button.directive.ts
+++ client/src/app/shared/decoration/button.directive.ts
@@ -1,29 +1,11 @@
-import { Directive, ElementRef, inject, Renderer2 } from '@angular/core';
+import { Directive } from '@angular/core';
 
 @Directive({
   selector: '[decorationButton]',
   standalone: true,
+  host: {
+    class: 'w-full bg-transparent border border-white/30 h-8 px-2.5 rounded-lg text-sm flex items-center',
+  },
 })
 export class DecorationButtonDirective {
-  renderer = inject(Renderer2);
-  hostElement = inject(ElementRef);
-
-  constructor() {
-    const classNames = [
-      'w-full',
-      'bg-transparent',
-      'border',
-      'border-white/30',
-      'h-8',
-      'px-2.5',
-      'rounded-lg',
-      'text-sm',
-      'flex',
-      'items-center',
-    ];
-
-    for (const className of classNames) {
-      this.renderer.addClass(this.hostElement.nativeElement, className);
-    }
-  }
 }
--- client/src/app/shared/decoration/input.directive.ts
+++ client/src/app/shared/decoration/input.directive.ts
@@ -1,27 +1,11 @@
-import { Directive, ElementRef, inject, Renderer2 } from '@angular/core';
+import { Directive } from '@angular/core';
 
 @Directive({
   selector: '[decorationInput]',
   standalone: true,
+  host: {
+    class: 'w-full bg-transparent border border-white/30 h-8 pl-2.5 rounded-lg text-sm',
+  },
 })
 export class DecorationInputDirective {
-  renderer = inject(Renderer2);
-  hostElement = inject(ElementRef);
-
-  constructor() {
-    const classNames = [
-      'w-full',
-      'bg-transparent',
-      'border',
-      'border-white/30',
-      'h-8',
-      'pl-2.5',
-      'rounded-lg',
-      'text-sm',
-    ];
-
-    for (const className of classNames) {
-      this.renderer.addClass(this.hostElement.nativeElement, className);
-    }
-  }
 }
